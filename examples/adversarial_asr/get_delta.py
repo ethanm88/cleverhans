8ffbfd4fe7a2dd50942c72928bf8db71e3ddcdb4
Ethan Mendes <eamendes88@gmail.com>
2020-06-29 12:24:45 -0400

    added graphs

--- examples/adversarial_asr/get_delta.py
+++ examples/adversarial_asr/get_delta.py
@@ -1,6 +1,6 @@
 import pickle
 
-file_name = 'adaptive_stage_1.pkl'
+file_name = 'final_adpative.pkl'
 pkl_file = open(file_name, 'rb')
 adv_example = pickle.load(pkl_file)
 print('Type', type(adv_example))

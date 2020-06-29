d65afa8966670b489fd03638c957801cb01309fb
Ethan Mendes <eamendes88@gmail.com>
2020-06-29 12:42:36 -0400

    added graphs

--- examples/adversarial_asr/get_delta.py
+++ examples/adversarial_asr/get_delta.py
@@ -1,10 +1,137 @@
 import pickle
-import numpy
+
+
+import numpy as np
+import scipy.io.wavfile as wav
+from absl import flags
+from absl import app
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string("root_dir", "./", "location of Librispeech")
+flags.DEFINE_string('input', 'read_data.txt',
+                    'Input audio .wav file(s), at 16KHz (separated by spaces)')
+
+# data processing
+flags.DEFINE_integer('window_size', '2048', 'window size in spectrum analysis')
+flags.DEFINE_integer('max_length_dataset', '223200',
+                     'the length of the longest audio in the whole dataset')
+flags.DEFINE_float('initial_bound', '2000', 'initial l infinity norm for adversarial perturbation')
+
+# training parameters
+flags.DEFINE_string('checkpoint', "./model/ckpt-00908156",
+                    'location of checkpoint')
+flags.DEFINE_integer('batch_size', '1', 'batch size')
+flags.DEFINE_float('lr_stage1', '100', 'learning_rate for stage 1')
+flags.DEFINE_float('lr_stage2', '1', 'learning_rate for stage 2')
+flags.DEFINE_integer('num_iter_stage1', '1000', 'number of iterations in stage 1')
+flags.DEFINE_integer('num_iter_stage2', '6000', 'number of iterations in stage 2')
+flags.DEFINE_integer('num_gpu', '0', 'which gpu to run')
+flags.DEFINE_float('factor', '-0.75', 'log of defensive perturbation proportionality factor k')
 
 file_name = 'final_adpative.pkl'
-pkl_file = open(file_name, 'rb')
-adv_example = pickle.load(pkl_file)
-print('Type', type(adv_example))
-print(numpy.array(adv_example).shape)
-print(adv_example)
-pkl_file.close()
+
+
+def ReadFromWav(data, batch_size):
+    """
+    Returns:
+        audios_np: a numpy array of size (batch_size, max_length) in float
+        trans: a numpy array includes the targeted transcriptions (batch_size, )
+        th_batch: a numpy array of the masking threshold, each of size (?, 1025)
+        psd_max_batch: a numpy array of the psd_max of the original audio (batch_size)
+        max_length: the max length of the batch of audios
+        sample_rate_np: a numpy array
+        masks: a numpy array of size (batch_size, max_length)
+        masks_freq: a numpy array of size (batch_size, max_length_freq, 80)
+        lengths: a list of the length of original audios
+    """
+    audios = []
+    lengths = []
+    th_batch = []
+    psd_max_batch = []
+    raw_audio = []
+
+    # read the .wav file
+    for i in range(batch_size):
+        sample_rate_np, audio_temp = wav.read(FLAGS.root_dir + str(data[0, i]))
+        raw_audio.append(audio_temp)
+        # read the wav form range from [-32767, 32768] or [-1, 1]
+        if max(audio_temp) < 1:
+            audio_np = audio_temp * 32768
+        else:
+            audio_np = audio_temp
+
+        length = len(audio_np)
+
+        audios.append(audio_np)
+        lengths.append(length)
+
+    max_length = max(lengths)
+
+    # pad the input audio
+    audios_np = np.zeros([batch_size, max_length])
+    masks = np.zeros([batch_size, max_length])
+    lengths_freq = (np.array(lengths) // 2 + 1) // 240 * 3
+    max_length_freq = max(lengths_freq)
+    masks_freq = np.zeros([batch_size, max_length_freq, 80])
+    for i in range(batch_size):
+        audio_float = audios[i].astype(float)
+        audios_np[i, :lengths[i]] = audio_float
+        masks[i, :lengths[i]] = 1
+        masks_freq[i, :lengths_freq[i], :] = 1
+
+        # compute the masking threshold
+        th, psd_max = generate_mask.generate_th(audios_np[i], sample_rate_np, FLAGS.window_size)
+        th_batch.append(th)
+        psd_max_batch.append(psd_max)
+
+    th_batch = np.array(th_batch)
+    psd_max_batch = np.array(psd_max_batch)
+
+    # read the transcription
+    trans = data[2, :]
+
+    return raw_audio, audios_np, trans, th_batch, psd_max_batch, max_length, sample_rate_np, masks, masks_freq, lengths
+
+def main(argv):
+    pkl_file = open(file_name, 'rb')
+    adv_example_lst = pickle.load(pkl_file)
+    print('Type', type(adv_example))
+    print(np.array(adv_example_lst[0]).shape)
+    print(adv_example_lst)
+    pkl_file.close()
+
+    data = np.loadtxt(FLAGS.input, dtype=str, delimiter=",")
+    data = data[:, FLAGS.num_gpu * 10: (FLAGS.num_gpu + 1) * 10]
+    num = len(data[0])
+    batch_size = FLAGS.batch_size
+    num_loops = num / batch_size
+    assert num % batch_size == 0
+
+    num_loops = 1
+    batch_size = 1
+    for l in range(num_loops):
+        data_sub = data[:, l * batch_size:(l + 1) * batch_size]
+
+        # stage 1
+        # all the output are numpy arrays
+        raw_audio, audios, trans, th_batch, psd_max_batch, maxlen, sample_rate, masks, masks_freq, lengths = ReadFromWav(
+            data_sub, batch_size)
+
+
+        for i in range(batch_size):
+            # save adv examples:
+            adv_example = adv_example_lst[0]
+            print("example: {}".format(i))
+
+
+            name, _ = data_sub[0, i].split(".")
+            saved_name = FLAGS.root_dir + str(name) + "_adaptive_stage2_actual.wav"
+            adv_example[i] = adv_example[i] / 32768.
+            print('size', np.array(adv_example[i][:lengths[i]]).shape)
+
+
+            wav.write(saved_name, 16000, np.array(adv_example[i][:lengths[i]]))
+            print(saved_name)
+
+if __name__ == '__main__':
+    app.run(main)

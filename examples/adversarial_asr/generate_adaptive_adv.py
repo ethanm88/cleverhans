ffb00c0e4acfc9c17dc2e285ba782866472e2405
Ethan Mendes <eamendes88@gmail.com>
2020-06-28 20:24:37 -0400

    fixed saving audio

--- examples/adversarial_asr/generate_adaptive_adv.py
+++ examples/adversarial_asr/generate_adaptive_adv.py
@@ -662,6 +662,8 @@ def main(argv):
                 file_name = 'adaptive_stage_1.pkl'
                 pkl_file = open(file_name, 'rb')
                 adv_example = pickle.load(pkl_file)
+                print('Type',type(adv_example))
+                print(adv_example)
                 pkl_file.close()
 
                 # stage 2

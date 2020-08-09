fix for the scaling

examples/adversarial_asr/generate_adaptive_legal.py
@@ -277,7 +277,7 @@ class Attack:
             self.rescale = tf.Variable(np.ones((batch_size, 1), dtype=np.float32) * FLAGS.initial_bound,
                                        name='qq_rescale')
 
-            self.rescale_th = tf.Variable(np.ones(batch_size, dtype=np.float32) * 2.0,
+            self.rescale_th = tf.Variable(np.ones(batch_size, dtype=np.float32) * 1.0,
                                           name='qq_resth')
 
 
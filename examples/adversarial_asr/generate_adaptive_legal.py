a180aa64bf5c67418c43eca5cf7e6c1dd17f87b8
Ethan Mendes <eamendes88@gmail.com>
2020-08-11 15:42:29 -0400

    added graph

--- examples/adversarial_asr/generate_adaptive_legal.py
+++ examples/adversarial_asr/generate_adaptive_legal.py
@@ -597,7 +597,7 @@ class Attack:
                     logits_delta = sess.run((self.transform((self.apply_delta[ii, :]), (self.psd_max_ori)[ii])), feed_dict)
                     thresh = sess.run((self.th[ii]), feed_dict)
 
-                    graph_data = {'logits': logits_delta, 'thresh': thresh}
+                    graph_data = {'logits': np.transpose(np.abs(logits_delta)), 'thresh': thresh}
 
 
                     file_name = 'graph_data.pkl'
